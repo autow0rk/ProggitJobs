a0466d427026632f7a581f3c7d6c8791061e0bdd
autow0rk <152362189+autow0rk@users.noreply.github.com>
2022-06-20 14:49:19 -0400

    Updated build script to be used for Dokku deploy. Compiles TS to JS, then runs node on entrypoint when deploying to Dokku.

--- backend/dist/util.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// function getMonthRangeForJobs(){ // returns past 3 months (including current month), as the only jobs that will be shown will be for the current month and the 2 previous months before that
-//     const currentDate = new Date();
-// }
