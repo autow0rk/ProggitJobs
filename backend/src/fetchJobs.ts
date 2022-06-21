07503a9f57d6f746620f5298edce6195132afd6f
autow0rk <152362189+autow0rk@users.noreply.github.com>
2022-06-20 21:24:36 -0400

    Cleaned up fetchJobs.ts file (removed unnecessary commented out code), and created app.json file to run cron job (of fetching jobs) in Dokku.

--- backend/src/fetchJobs.ts
+++ backend/src/fetchJobs.ts
@@ -1,8 +1,4 @@
-// const snoowrap = require('snoowrap');
-// import {Snoowrap} from 'snoowrap';
 import Snoowrap = require("snoowrap");
-// import getScalaJobPosts from './subreddits/scala'
-// import getReactJSJobPosts from "./subreddits/reactJS";
 import scalaJobThreadFilter from "./subreddits/filters/scala";
 import reactJobThreadFilter from "./subreddits/filters/reactJS";
 import { SearchOptions } from "snoowrap";
@@ -30,9 +26,6 @@ const searchOptionsReactJSJobs : SearchOptions = {
   limit:3
 }
 
-
-
-
   try { // if there's any issues in making our Reddit client or making Reddit API calls, we want to fail gracefully. We don't want some obscure error taking down the whole server, so we wrap the Reddit API logic in a try block.
     let userAgentValue: string;
     if(process.env.USER_AGENT) {
@@ -47,13 +40,11 @@ const searchOptionsReactJSJobs : SearchOptions = {
       username: process.env.REDDIT_USERNAME,
       password: process.env.REDDIT_PASSWORD
     });
-    // getJobPostsForSubreddit(reddit_client, 'scala', searchOptionsScalaJobs, scalaJobThreadFilter);
     const scalaJobRecords = (await getJobPostsForSubreddit(reddit_client, 'scala', searchOptionsScalaJobs, scalaJobThreadFilter)).flat();
       await prisma.job.createMany({
         data: scalaJobRecords,
         skipDuplicates: true
       });
-
     
 
     const reactJSJobRecords = (await getJobPostsForSubreddit(reddit_client, 'reactjs', searchOptionsReactJSJobs, reactJobThreadFilter)).flat();
@@ -62,52 +53,10 @@ const searchOptionsReactJSJobs : SearchOptions = {
       skipDuplicates: true
     });
 
-    // const reactRecords = await prisma.job.findMany({
-    //   where: {
-    //     fromSubreddit: {
-    //       equals: 'react'
-    //     }
-    //   }
-    // });
-
-    // console.log(reactRecords);
-
-
-    
-
-      
-
-      // const reactJSJobRecords = await getReactJSJobPosts(reddit_client, 'reactjs', searchOptionsReactJSJobs);
-      //  console.log(reactJSJobRecords.length);
-      // await prisma.job.createMany({
-      //   data: reactJSJobRecords,
-      //   skipDuplicates: true
-      // });
-
-      // console.log('original date: ', results[results.length - 1].date);
-      // console.log('converted to local time date: ', utcToZonedTime(results[results.length - 1].date, timezone))
-      // const scalaJobRecords = await Promise.all(results.map(async (jobPosting) => {
-      //   const newScalaJob = await prisma.job.create({
-      //     data: {
-      //       fromSubreddit: jobPosting.fromSubreddit,
-      //       datePosted: jobPosting.datePosted,
-      //       jobPostingDetails: jobPosting.jobPostingDetails,
-      //       url: jobPosting.url
-      //     }
-      //   })
-      // }));
-      // console.log('lets check out all the scala job records: ', scalaJobRecords);
-      // const results2 = await getReactJSJobPosts(reddit_client, 'reactjs', searchOptionsReactJSJobs);
-      // console.log('react results: ', results2);
   } catch (err) {
     console.log('caught an error: ', err)
   }
 
 }
 
-
 fetchJobs();
-// A = fetchJobs
-// B = scala
-
-// fetchJobs imports scala's scraper function, scala imports fetchJob's Snoowrap type?
